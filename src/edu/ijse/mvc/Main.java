b3bcb78bbe17db15fdb3da908b2f8412c1a4e203
Shenori Ruwesha <shenoriperera87@gmail.com>
2026-01-23 12:52:51 +0530

    Upload the maven project

---
+++ src/edu/ijse/mvc/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package edu.ijse.mvc;
+
+/**
+ *
+ * @author Asus
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        // TODO code application logic here
+    }
+    
+}
