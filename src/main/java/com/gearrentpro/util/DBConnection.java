fccc50182f94936aa4f5a1887da831d866a4ab46
Shenori Ruwesha <shenoriperera87@gmail.com>
2026-05-09 13:06:47 +0530

    DBConnection: Singleton JDBC connection to MySQL (gearrentpro DB)

--- src/main/java/com/gearrentpro/util/DBConnection.java
+++ src/main/java/com/gearrentpro/util/DBConnection.java
@@ -1,18 +1,18 @@
-package com.gearrentpro.util;
+package main.java.com.gearrentpro.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    
+
     private static DBConnection instance;
     private Connection connection;
-    
+
     private static final String URL = "jdbc:mysql://localhost:3306/gearrentpro";
     private static final String USER = "root";
-    private static final String PASSWORD = "your_password"; // Change this
-    
+    private static final String PASSWORD = "Shenori123"; // Change to your MySQL password
+
     private DBConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -21,14 +21,14 @@ public class DBConnection {
             throw new SQLException("MySQL JDBC Driver not found", e);
         }
     }
-    
+
     public static DBConnection getInstance() throws SQLException {
         if (instance == null || instance.getConnection().isClosed()) {
             instance = new DBConnection();
         }
         return instance;
     }
-    
+
     public Connection getConnection() {
         return connection;
     }
